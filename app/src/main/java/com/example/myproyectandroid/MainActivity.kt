6da6f19cdf60b2f1050337dbe0836e0db99eaf55
andres <desarrollolibre.net@gmail.com>
2026-01-18 06:01:06 -0400

    FAB

--- app/src/main/java/com/example/myproyectandroid/MainActivity.kt
+++ app/src/main/java/com/example/myproyectandroid/MainActivity.kt
@@ -21,11 +21,20 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.LargeFloatingActionButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myproyectandroid.ui.theme.MyProyectAndroidTheme
@@ -53,6 +62,52 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+}
+
+@Composable
+fun ExamplesFAB() {
+//    Button(onClick = { /* Acción */ }) {
+//        Text("Aceptar")
+//    }
+    Column(
+        modifier = Modifier.fillMaxWidth().padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(20.dp)
+    ) {
+        // 1. FAB SMALL
+        SmallFloatingActionButton(
+            onClick = { },
+            containerColor = MaterialTheme.colorScheme.secondaryContainer,
+            contentColor = MaterialTheme.colorScheme.secondary
+        ) {
+            Icon(Icons.Filled.Add, "Small floating action button.")
+        }
+
+        // 2. FAB Large
+        LargeFloatingActionButton(
+            onClick = { },
+//            shape = CutCornerShape(1.dp)
+//            shape = RectangleShape
+            shape = RoundedCornerShape(15.dp)
+//            shape = CircleShape,
+        ) {
+            Icon(Icons.Filled.Add, "Large floating action button")
+        }
+
+        // 3. FAB + TEXT
+        ExtendedFloatingActionButton(
+            onClick = { },
+            icon = { Icon(Icons.Filled.Add, "Extended floating action button.") },
+            text = { Text(text = "Extended FAB") },
+        )
+
+        // 4. FAB
+        FloatingActionButton(
+            onClick = {  },
+        ) {
+            Icon(Icons.Filled.Add, "Floating action button.")
+        }
+
+    }
 }
 
 @Composable
@@ -98,6 +153,7 @@ fun ExamplesButtons() {
 fun GreetingPreview() {
     MyProyectAndroidTheme {
        // Greeting("Andasasasroid")
-        ExamplesButtons()
+//        ExamplesButtons()
+        ExamplesFAB()
     }
 }
