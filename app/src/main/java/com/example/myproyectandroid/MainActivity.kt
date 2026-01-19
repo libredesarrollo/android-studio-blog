383e3222ff654b8094f144686a2c9c9d36191e48
andres <desarrollolibre.net@gmail.com>
2026-01-19 06:38:48 -0400

    Dialogos

--- app/src/main/java/com/example/myproyectandroid/MainActivity.kt
+++ app/src/main/java/com/example/myproyectandroid/MainActivity.kt
@@ -1,40 +1,30 @@
 package com.example.myproyectandroid
 
 
+import android.app.AlertDialog
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Button
-import androidx.compose.material3.FilledTonalButton
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.TextButton
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.CutCornerShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ExtendedFloatingActionButton
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.LargeFloatingActionButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SmallFloatingActionButton
+
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myproyectandroid.ui.theme.MyProyectAndroidTheme
@@ -46,6 +36,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyProyectAndroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    //EjemploDialogoCompose()
+                    EjemploDialogoListCompose()
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
@@ -56,104 +48,108 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Composable
-fun ExamplesFAB() {
-//    Button(onClick = { /* Acción */ }) {
-//        Text("Aceptar")
-//    }
-    Column(
-        modifier = Modifier.fillMaxWidth().padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(20.dp)
-    ) {
-        // 1. FAB SMALL
-        SmallFloatingActionButton(
-            onClick = { },
-            containerColor = MaterialTheme.colorScheme.secondaryContainer,
-            contentColor = MaterialTheme.colorScheme.secondary
-        ) {
-            Icon(Icons.Filled.Add, "Small floating action button.")
+    @Composable
+    fun EjemploDialogoCompose() {
+        // 1. Definimos el estado (¿se muestra el diálogo?)
+        var showDialog by remember { mutableStateOf(false) }
+
+        // Botón que activa el diálogo
+        Button(onClick = { showDialog = true }) {
+            Text("Eliminar elemento")
         }
 
-        // 2. FAB Large
-        LargeFloatingActionButton(
-            onClick = { },
-//            shape = CutCornerShape(1.dp)
-//            shape = RectangleShape
-            shape = RoundedCornerShape(15.dp)
-//            shape = CircleShape,
-        ) {
-            Icon(Icons.Filled.Add, "Large floating action button")
+        // 2. Declaramos el Diálogo
+        if (showDialog) {
+            AlertDialog(
+                onDismissRequest = {
+                    // Se ejecuta cuando el usuario toca fuera del diálogo o pulsa "Atrás"
+                    showDialog = false
+                },
+                title = {
+                    Text(text = "Confirmar eliminación")
+                },
+                text = {
+                    Text(text = "¿Estás seguro de que deseas borrar este archivo permanentemente?")
+                },
+                confirmButton = {
+                    TextButton(onClick = {
+                        showDialog = false
+                        // Aquí iría tu lógica de borrado
+                    }) {
+                        Text("Confirmar")
+                    }
+                },
+                dismissButton = {
+                    TextButton(onClick = { showDialog = false }) {
+                        Text("Cancelar")
+                    }
+                }
+            )
         }
+    }
 
-        // 3. FAB + TEXT
-        ExtendedFloatingActionButton(
-            onClick = { },
-            icon = { Icon(Icons.Filled.Add, "Extended floating action button.") },
-            text = { Text(text = "Extended FAB") },
+@Composable
+fun EjemploDialogoListCompose() {
+    // 1. Definimos el estado (¿se muestra el diálogo?)
+    var showDialog by remember { mutableStateOf(false) }
+
+    val items = listOf("Opción 1", "Opción 2", "Opción 3")
+
+    // Botón que activa el diálogo
+    Button(onClick = { showDialog = true }) {
+        Text("Eliminar elemento")
+    }
+
+    // 2. Declaramos el Diálogo
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = {
+                // Se ejecuta cuando el usuario toca fuera del diálogo o pulsa "Atrás"
+                showDialog = false
+            },
+            title = {
+                Text(text = "Confirmar eliminación")
+            },
+            text = {
+                LazyColumn {
+                    items(items) { item ->
+                        Text(
+                            text = item,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clickable {
+                                    // Acción al seleccionar un item
+                                    println("$item seleccionado")
+                                }
+                                .padding(vertical = 12.dp)
+                        )
+                    }
+                }
+            },
+            confirmButton = {
+                TextButton(onClick = {
+                    showDialog = false
+                    // Aquí iría tu lógica de borrado
+                }) {
+                    Text("Confirmar")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = { showDialog = false }) {
+                    Text("Cancelar")
+                }
+            }
         )
-
-        // 4. FAB
-        FloatingActionButton(
-            onClick = {  },
-        ) {
-            Icon(Icons.Filled.Add, "Floating action button.")
-        }
-
     }
 }
 
-@Composable
-fun ExamplesButtons() {
-//    Button(onClick = { /* Acción */ }) {
-//        Text("Aceptar")
-//    }
-    Column(
-        modifier = Modifier.fillMaxWidth().padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(20.dp)
-    ) {
-        // 1. Botón Principal (Contained)
-        Button(onClick = { /* Acción */ }) {
-            Text("Aceptar")
-        }
 
-        // 2. Botón con Tono (Menos intenso)
-        FilledTonalButton(onClick = { /* Acción */ }) {
-            Text("Guardar Borrador")
-        }
-
-        // 3. Botón con Borde (Outlined)
-        OutlinedButton(onClick = { /* Acción */ }) {
-            Text("Editar Perfil")
-        }
-
-        // 4. Botón de Texto
-        TextButton(onClick = { /* Acción */ }) {
-            Text("Cancelar")
-        }
-
-        // 5. Botón con Icono
-        Button(onClick = { /* Acción */ }) {
-            Icon(Icons.Default.Add, contentDescription = null)
-            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
-            Text("Cancelar")
-        }
-    }
-}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun Preview() {
     MyProyectAndroidTheme {
-       // Greeting("Andasasasroid")
-//        ExamplesButtons()
-        ExamplesFAB()
+        EjemploDialogoCompose()
     }
 }
